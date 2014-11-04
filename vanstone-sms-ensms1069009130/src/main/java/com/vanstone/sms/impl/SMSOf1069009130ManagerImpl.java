2f88d8f4920300cb8e4bf321eb2c5e1c7eede0c2
soltex <shipengpipi@126.com>
2014-11-05 00:30:13 +0800

    1.2.8 master

--- vanstone-sms-ensms1069009130/src/main/java/com/vanstone/sms/impl/SMSOf1069009130ManagerImpl.java
+++ vanstone-sms-ensms1069009130/src/main/java/com/vanstone/sms/impl/SMSOf1069009130ManagerImpl.java
@@ -4,24 +4,23 @@
 package com.vanstone.sms.impl;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
-import com.vanstone.commons.common.Assert;
-import com.vanstone.commons.common.component.queue.GetMethod;
 import com.vanstone.sms.C106ChannelCode;
 import com.vanstone.sms.ConfObject;
-import com.vanstone.sms.DateUtil;
 import com.vanstone.sms.ISMSOf1069009130Manager;
 import com.vanstone.sms.MsgUtil;
 
@@ -55,13 +54,14 @@ public class SMSOf1069009130ManagerImpl implements ISMSOf1069009130Manager {
 		
 		for (String s : arrays) {
 			C106MTObject mtObject = new C106MTObject(mtUsername, mtPassword, mobiles, s, port);
-			GetMethod getMethod = new GetMethod(mtAddress + mtObject.toRequestQuery());
+			HttpGet getMethod = new HttpGet(mtAddress + mtObject.toRequestQuery());
 			C106ChannelCode smsChannelCode = null;
-			HttpClient httpClient = new HttpClient();
+			HttpClient httpClient = new DefaultHttpClient();
 			try {
-				int statusCode = httpClient.executeMethod(getMethod);
+				HttpResponse httpResponse = httpClient.execute(getMethod);
+				int statusCode = httpResponse.getStatusLine().getStatusCode();
 				if (statusCode == HttpStatus.SC_OK) {
-					String content = getMethod.getResponseBodyAsString();
+					String content = EntityUtils.toString(httpResponse.getEntity());
 					if (!StringUtils.isEmpty(content)) {
 						smsChannelCode = C106ChannelCode.retrievalObject(Integer.parseInt(content));
 					}else {
@@ -72,13 +72,13 @@ public class SMSOf1069009130ManagerImpl implements ISMSOf1069009130Manager {
 					_LOG.error("Send Message ERROR, Mobile -> " + mtObject.toRequestQuery());
 				}
 				c106ChannelCodes.add(smsChannelCode);
-			} catch (HttpException e) {
+			} catch (ParseException e) {
 				e.printStackTrace();
 				throw new IllegalArgumentException(e);
 			} catch (IOException e) {
 				e.printStackTrace();
 				throw new IllegalArgumentException(e);
-			} finally {
+			}  finally {
 				if (getMethod != null) {
 					getMethod.releaseConnection();
 				}
@@ -101,27 +101,28 @@ public class SMSOf1069009130ManagerImpl implements ISMSOf1069009130Manager {
 		String mtUsername = ConfObject.getInstance().getC106Username();
 		String mtPassword = ConfObject.getInstance().getC106Password();
 		
-		HttpClient httpClient = new HttpClient();
+		HttpClient httpClient = new DefaultHttpClient();
 		
 		BaseC106SMSChannelConf mtObject = new BaseC106SMSChannelConf(mtUsername, mtPassword);
-		GetMethod getMethod = new GetMethod(inventoriesAddress + mtObject.toRequestQuery());
+		HttpGet getMethod = new HttpGet(inventoriesAddress + mtObject.toRequestQuery());
 		try {
-			int statusCode = httpClient.executeMethod(getMethod);
+			HttpResponse httpResponse = httpClient.execute(getMethod);
+			int statusCode = httpResponse.getStatusLine().getStatusCode();
 			if (statusCode == HttpStatus.SC_OK) {
-				String content = getMethod.getResponseBodyAsString();
+				String content = EntityUtils.toString(httpResponse.getEntity());
 				if (!StringUtils.isEmpty(content)) {
 					return Integer.parseInt(content.trim());
 				}
 			}
 			_LOG.error("SEND SMS -> HTTP CODE : " + statusCode);
 			throw new IllegalArgumentException("http error " + statusCode);
-		} catch (HttpException e) {
+		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 			throw new IllegalArgumentException(e);
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new IllegalArgumentException(e);
-		} finally {
+		}  finally {
 			if (getMethod != null) {
 				getMethod.releaseConnection();
 			}
@@ -129,96 +130,96 @@ public class SMSOf1069009130ManagerImpl implements ISMSOf1069009130Manager {
 		}
 	}
 
-	@Override
-	public boolean sendHaottMsg(String mobile, String content) {
-		Assert.hasText(mobile);
-		Assert.hasText(content);
-		String haottMTAddress = ConfObject.getInstance().getHaottMTAddress();
-		Assert.hasText(haottMTAddress);
-		HttpClient httpClient = new HttpClient();
-		GetMethod getMethod = null;
-		try {
-			content = URLEncoder.encode(content, "utf-8");
-			StringBuffer sb = new StringBuffer();
-			sb.append("mbls=").append(mobile).append("&");
-			sb.append("content=").append(content);
-			haottMTAddress = haottMTAddress + "?" + sb.toString();
-			getMethod = new GetMethod(haottMTAddress);
-			int code = httpClient.executeMethod(getMethod);
-			if (code == HttpStatus.SC_OK) {
-				return true;
-			}
-			return false;
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return false;
-		} catch (HttpException e) {
-			e.printStackTrace();
-			return false;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		} finally {
-			if (getMethod != null) {
-				getMethod.releaseConnection();
-			}
-			httpClient = null;
-		}
-	}
-
-	@Override
-	public boolean sendHaottMsg(List<String> mobiles, String content) {
-		return false;
-	}
-	
-	@Override
-	public List<HaottMOObject> getHaottMOObjects() {
-		String mtaddress = ConfObject.getInstance().getHaottMOAddress();
-		Assert.hasText(mtaddress);
-		HttpClient httpClient = new HttpClient();
-		GetMethod getMethod = null;
-		try {
-			getMethod = new GetMethod(mtaddress);
-			int code = httpClient.executeMethod(getMethod);
-			if (code == HttpStatus.SC_OK) {
-				String responseBody = new String(getMethod.getResponseBody(),"utf-8");
-				if (responseBody == null || responseBody.trim().equals("-1")) {
-					return null;
-				}
-				
-				//字符串解析   18610558506,2013-11-29 14:37:19,测试<br/>
-				responseBody = responseBody.trim();
-				String[] strs = responseBody.split("<br/>");
-				List<HaottMOObject> tts = new ArrayList<HaottMOObject>();
-				for (String string : strs) {
-					String[] array = string.split(",");
-					String mobile = array[0];
-					Date time = DateUtil.string2Date(array[1], "yyyy-MM-dd HH:mm:ss");
-					String content = array[2];
-					HaottMOObject mtObject = new HaottMOObject();
-					mtObject.setMobile(mobile);
-					mtObject.setMsg(content);
-					mtObject.setMoTime(time);
-					tts.add(mtObject);
-				}
-				return tts;
-			}
-			return null;
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-			return null;
-		} catch (HttpException e) {
-			e.printStackTrace();
-			return null;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		} finally {
-			if (httpClient != null) {
-				getMethod.releaseConnection();
-			}
-			httpClient = null;
-		}
-	}
+//	@Override
+//	public boolean sendHaottMsg(String mobile, String content) {
+//		Assert.hasText(mobile);
+//		Assert.hasText(content);
+//		String haottMTAddress = ConfObject.getInstance().getHaottMTAddress();
+//		Assert.hasText(haottMTAddress);
+//		HttpClient httpClient = new HttpClient();
+//		GetMethod getMethod = null;
+//		try {
+//			content = URLEncoder.encode(content, "utf-8");
+//			StringBuffer sb = new StringBuffer();
+//			sb.append("mbls=").append(mobile).append("&");
+//			sb.append("content=").append(content);
+//			haottMTAddress = haottMTAddress + "?" + sb.toString();
+//			getMethod = new GetMethod(haottMTAddress);
+//			int code = httpClient.executeMethod(getMethod);
+//			if (code == HttpStatus.SC_OK) {
+//				return true;
+//			}
+//			return false;
+//		} catch (UnsupportedEncodingException e) {
+//			e.printStackTrace();
+//			return false;
+//		} catch (HttpException e) {
+//			e.printStackTrace();
+//			return false;
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			return false;
+//		} finally {
+//			if (getMethod != null) {
+//				getMethod.releaseConnection();
+//			}
+//			httpClient = null;
+//		}
+//	}
+//
+//	@Override
+//	public boolean sendHaottMsg(List<String> mobiles, String content) {
+//		return false;
+//	}
+//	
+//	@Override
+//	public List<HaottMOObject> getHaottMOObjects() {
+//		String mtaddress = ConfObject.getInstance().getHaottMOAddress();
+//		Assert.hasText(mtaddress);
+//		HttpClient httpClient = new HttpClient();
+//		GetMethod getMethod = null;
+//		try {
+//			getMethod = new GetMethod(mtaddress);
+//			int code = httpClient.executeMethod(getMethod);
+//			if (code == HttpStatus.SC_OK) {
+//				String responseBody = new String(getMethod.getResponseBody(),"utf-8");
+//				if (responseBody == null || responseBody.trim().equals("-1")) {
+//					return null;
+//				}
+//				
+//				//字符串解析   18610558506,2013-11-29 14:37:19,测试<br/>
+//				responseBody = responseBody.trim();
+//				String[] strs = responseBody.split("<br/>");
+//				List<HaottMOObject> tts = new ArrayList<HaottMOObject>();
+//				for (String string : strs) {
+//					String[] array = string.split(",");
+//					String mobile = array[0];
+//					Date time = DateUtil.string2Date(array[1], "yyyy-MM-dd HH:mm:ss");
+//					String content = array[2];
+//					HaottMOObject mtObject = new HaottMOObject();
+//					mtObject.setMobile(mobile);
+//					mtObject.setMsg(content);
+//					mtObject.setMoTime(time);
+//					tts.add(mtObject);
+//				}
+//				return tts;
+//			}
+//			return null;
+//		} catch (UnsupportedEncodingException e) {
+//			e.printStackTrace();
+//			return null;
+//		} catch (HttpException e) {
+//			e.printStackTrace();
+//			return null;
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			return null;
+//		} finally {
+//			if (httpClient != null) {
+//				getMethod.releaseConnection();
+//			}
+//			httpClient = null;
+//		}
+//	}
 
 }
